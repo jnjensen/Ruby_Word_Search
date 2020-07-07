1ee2dd210db4f6fc3569d2f6dee50866c56e7f1c
James <jnjensen@live.com>
2020-07-06 22:07:47 -0500

    Completed sub string project

--- sub_string.rb
+++ sub_string.rb
@@ -1,3 +1,33 @@
-def counting_strings (str, dictionary)
+def case_erase (obj)
+    if obj.is_a?(Array)
+        obj = obj.join(" ")
+    end
 
+    obj = obj.downcase
+    obj = obj.split(" ")
+    return obj
 end
+
+def counting_strings (str, dictionary)
+    new_array = case_erase(str)
+    dictionary = case_erase(dictionary)
+    new_hash = Hash.new
+    
+    dictionary.each do |key|
+        num = 0
+        new_array.each do |compare|
+            if compare.include?(key)
+                num += 1
+            end
+        end
+        if num >= 1
+            new_hash[key] = num
+        end
+    end
+
+    return new_hash
+end
+
+dictionary = ["Boo", "who", "you", "boy", "bored", "bo"]
+
+puts counting_strings("boo who", dictionary)
